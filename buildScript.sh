18185e64aa9b887b9345e1347852b8927eaca38a
Celio1212 <celiopereira751@gmail.com>
2022-08-10 14:41:44 +0100

    Second commit

---
+++ buildScript.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+echo "running from a script! :0"
+echo "Building app 0%"
+echo "Building app 50%"
+echo "Building app 100%"
+touch coolApp.exe
+
+echo "running from a script! :1"
+echo "Building file 0%"
+echo "Building file 50%"
+echo "Building file 100%"
+mkdir /oops
+
+echo "running from a script! :2"
+echo "Moving app 0%"
+echo "Moving app 50%"
+echo "Moving app 100%"
+mv coolApp.exe /oops
+
+echo "running from a script! :3"
+echo "error found on file 0%"
+echo "error found on file app 50%"
+echo "error found on file app 100%"
+echo "Removing file"
+rm -d /oops
---
+++ buildScript1.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+Location: pwd
+echo Location
+
+User: whoami
+echo User
+
+touch randomtext.txt
+echo "Hello!" >> randomtext.txt
+
+echo ls -a
