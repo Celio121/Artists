95778430a71d0f85f1faa2d6579e971766e294fe
Celio1212 <celiopereira751@gmail.com>
2022-08-10 14:55:36 +0100

    Third Commit

--- buildScript.sh
+++ buildScript.sh
@@ -4,23 +4,23 @@ echo "running from a script! :0"
 echo "Building app 0%"
 echo "Building app 50%"
 echo "Building app 100%"
-touch coolApp.exe
+sudo touch coolApp.exe
 
 echo "running from a script! :1"
 echo "Building file 0%"
 echo "Building file 50%"
 echo "Building file 100%"
-mkdir /oops
+sudo mkdir /oops
 
 echo "running from a script! :2"
 echo "Moving app 0%"
 echo "Moving app 50%"
 echo "Moving app 100%"
-mv coolApp.exe /oops
+sudo mv coolApp.exe /oops
 
 echo "running from a script! :3"
 echo "error found on file 0%"
 echo "error found on file app 50%"
 echo "error found on file app 100%"
 echo "Removing file"
-rm -d /oops
+sudo rm -d /oops
